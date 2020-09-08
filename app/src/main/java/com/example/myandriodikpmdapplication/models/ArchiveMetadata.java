1bb7b70f06a285f99216c2723224c2bd131f9fa6
Anthony <info@anthonyscheeres.nl>
2020-09-09 00:24:54 +0200

    Starting to build on the comic book app

---
+++ app/src/main/java/com/example/myandriodikpmdapplication/models/ArchiveMetadata.java
@@ -0,0 +1,104 @@
+package com.example.myandriodikpmdapplication.models;
+
+import java.util.ArrayList;
+
+public class ArchiveMetadata {
+
+        private float created;
+        private String d1;
+        private String d2;
+        private String dir;
+        ArrayList< Object > files = new ArrayList < Object > ();
+        private float files_count;
+        private float item_last_updated;
+        private float item_size;
+        Metadata MetadataObject;
+        private String server;
+        private float uniq;
+        ArrayList < Object > workable_servers = new ArrayList < Object > ();
+
+
+        // Getter Methods
+
+        public float getCreated() {
+            return created;
+        }
+
+        public String getD1() {
+            return d1;
+        }
+
+        public String getD2() {
+            return d2;
+        }
+
+        public String getDir() {
+            return dir;
+        }
+
+        public float getFiles_count() {
+            return files_count;
+        }
+
+        public float getItem_last_updated() {
+            return item_last_updated;
+        }
+
+        public float getItem_size() {
+            return item_size;
+        }
+
+        public Metadata getMetadata() {
+            return MetadataObject;
+        }
+
+        public String getServer() {
+            return server;
+        }
+
+        public float getUniq() {
+            return uniq;
+        }
+
+        // Setter Methods
+
+        public void setCreated(float created) {
+            this.created = created;
+        }
+
+        public void setD1(String d1) {
+            this.d1 = d1;
+        }
+
+        public void setD2(String d2) {
+            this.d2 = d2;
+        }
+
+        public void setDir(String dir) {
+            this.dir = dir;
+        }
+
+        public void setFiles_count(float files_count) {
+            this.files_count = files_count;
+        }
+
+        public void setItem_last_updated(float item_last_updated) {
+            this.item_last_updated = item_last_updated;
+        }
+
+        public void setItem_size(float item_size) {
+            this.item_size = item_size;
+        }
+
+        public void setMetadata(Metadata metadataObject) {
+            this.MetadataObject = metadataObject;
+        }
+
+        public void setServer(String server) {
+            this.server = server;
+        }
+
+        public void setUniq(float uniq) {
+            this.uniq = uniq;
+        }
+    }
---
+++ app/src/main/java/com/example/myandriodikpmdapplication/models/Metadata.java
@@ -0,0 +1,140 @@
+package com.example.myandriodikpmdapplication.models;
+
+import java.util.ArrayList;
+
+public class Metadata {
+        private String identifier;
+        ArrayList < Object > collection = new ArrayList < Object > ();
+        private String description;
+        private String language;
+        private String mediatype;
+        private String scanner;
+        private String title;
+        private String publicdate;
+        private String uploader;
+        private String addeddate;
+        private String identifier_access;
+        private String identifier_ark;
+        private String ocr;
+        private String coverleaf;
+        private String backup_location;
+
+
+        // Getter Methods
+
+        public String getIdentifier() {
+            return identifier;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public String getLanguage() {
+            return language;
+        }
+
+        public String getMediatype() {
+            return mediatype;
+        }
+
+        public String getScanner() {
+            return scanner;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getPublicdate() {
+            return publicdate;
+        }
+
+        public String getUploader() {
+            return uploader;
+        }
+
+        public String getAddeddate() {
+            return addeddate;
+        }
+
+        public String getIdentifier_access() {
+            return identifier_access;
+        }
+
+        public String getIdentifier_ark() {
+            return identifier_ark;
+        }
+
+        public String getOcr() {
+            return ocr;
+        }
+
+        public String getCoverleaf() {
+            return coverleaf;
+        }
+
+        public String getBackup_location() {
+            return backup_location;
+        }
+
+        // Setter Methods
+
+        public void setIdentifier(String identifier) {
+            this.identifier = identifier;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public void setLanguage(String language) {
+            this.language = language;
+        }
+
+        public void setMediatype(String mediatype) {
+            this.mediatype = mediatype;
+        }
+
+        public void setScanner(String scanner) {
+            this.scanner = scanner;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public void setPublicdate(String publicdate) {
+            this.publicdate = publicdate;
+        }
+
+        public void setUploader(String uploader) {
+            this.uploader = uploader;
+        }
+
+        public void setAddeddate(String addeddate) {
+            this.addeddate = addeddate;
+        }
+
+        public void setIdentifier_access(String identifier - access) {
+            this.identifier_access = identifier - access;
+        }
+
+        public void setIdentifier_ark(String identifier - ark) {
+            this.identifier_ark = identifier - ark;
+        }
+
+        public void setOcr(String ocr) {
+            this.ocr = ocr;
+        }
+
+        public void setCoverleaf(String coverleaf) {
+            this.coverleaf = coverleaf;
+        }
+
+        public void setBackup_location(String backup_location) {
+            this.backup_location = backup_location;
+        }
+
+
+}
