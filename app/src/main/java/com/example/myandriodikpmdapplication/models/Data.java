f5dc272bb50f5df8c0b1feb48a9a42cb9ef2ad05
Anthony <info@anthonyscheeres.nl>
2020-09-10 19:08:01 +0200

    .

---
+++ app/src/main/java/com/example/myandriodikpmdapplication/models/Data.java
@@ -0,0 +1,8 @@
+package com.example.myandriodikpmdapplication.models;
+
+import java.util.ArrayList;
+
+public class Data {
+    public static ArrayList<Object> docs;
+
+}
--- app/src/main/java/com/example/myandriodikpmdapplication/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/myandriodikpmdapplication/ui/home/HomeViewModel.java
@@ -5,18 +5,23 @@ import androidx.lifecycle.ViewModel;
 import com.example.myandriodikpmdapplication.interfaces.Archive;
 import com.example.myandriodikpmdapplication.interfaces.Http;
 import com.example.myandriodikpmdapplication.models.ArchiveManga;
+import com.example.myandriodikpmdapplication.models.Data;
 import com.example.myandriodikpmdapplication.services.ArchiveOrgUrlService;
 import com.example.myandriodikpmdapplication.services.HttpService;
 import com.example.myandriodikpmdapplication.services.JoinExample;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class HomeViewModel extends ViewModel {
 
 
     AtomicReference<ArrayList<Object>> docsz;
+
+    ArrayList<Object> docs = null;
+
+
+
 
     public HomeViewModel() {
 
@@ -28,43 +33,49 @@ public class HomeViewModel extends ViewModel {
 
         Http http = new HttpService();
 
-        Archive archive= new ArchiveOrgUrlService();
+        Archive archive = new ArchiveOrgUrlService();
 
 
-        ArrayList<Object> docs = null;
-try{
+        try {
+
+            Thread thread = new Thread(){
 
 
-
-    JoinExample runnable=new JoinExample();
-
+                public void run(){
 
 
-    Thread t1 = new Thread(runnable);
+                    ArchiveManga collectionOfComics = null;
+                    try {
+                        collectionOfComics = archive.search("webcomicuniverse", http);
+                        System.out.println("idiot zzzzzzzzzzzzzzz");
+                    } catch (Exception e) {
 
-    t1.start();
-    
-        //change to manga_library to turn it into manga app
-
-
-              ArchiveManga collectionOfComics = archive.search("webcomicuniverse", http);
-
+                    }
 
 
-  docs = collectionOfComics.getResponse().getDocs();
-                t1.join();
+                    Data.docs = collectionOfComics.getResponse().getDocs();
 
-            } catch (Exception e) {
-    System.out.println("idiot zz");
-            }
 
-if (docs==null){
+                }
+            };
 
-    docs = new ArrayList<Object>();
+            thread.start();
 
-}
+            thread.join();
 
-return docs;
+            docs = Data.docs;
+
+        } catch (Exception e) {
+            System.out.println("idiot zz");
+        }
+
+        if (docs == null) {
+
+            docs = new ArrayList<Object>();
+
+        }
+
+        return docs;
 
     }
 
